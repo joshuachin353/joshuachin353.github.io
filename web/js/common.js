d2a2271c51926d16885b9b6bee49db06390ec84a
Chow Lip Heng <lipheng233146@dasein.edu.my>
2026-08-22 20:24:10 +0800

    refactor: adjusted mobile sub-menu css in common.css

--- web/js/common.js
+++ web/js/common.js
@@ -6,27 +6,5 @@ gsap.from("nav a", {
     duration: 0.8,
     ease: "power2.in"
 });
-function toggleMobileMenu() {
-    const mobileDrawer = document.querySelector('.mb-sub-menu-bar');
-    const menuToggle = document.querySelector('.menu-toggle');
 
-    if (mobileDrawer) {
-        mobileDrawer.classList.toggle('active');
-    }
-
-    if (menuToggle) {
-        const icon = menuToggle.querySelector('i');
-        if (icon) {
-            icon.classList.toggle('fa-bars');
-            icon.classList.toggle('fa-xmark');
-        }
-    }
-}
-
-function toggleAccordion(buttonElement) {
-    const parentAccordion = buttonElement.parentElement;
-    if (parentAccordion) {
-        parentAccordion.classList.toggle('open');
-    }
-}
 
